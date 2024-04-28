c51c643fd2a43a76bff6e78dce8ca6ad5edc2b8a
Shivani <shivanigupta25696@gmail.com>
2024-04-28 08:45:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".navbar");
+
+const toggleNavbar = () => {
+  nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  navText:[
+    '<i class="fa fa-angle-left"></i>',
+    '<i class="fa fa-angle-right"></i>'
+  ],
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+
+const thumbnails = document.querySelectorAll('.gallery-item');
+thumbnails.forEach(thumbnail => {
+  thumbnail.addEventListener('click', function() {
+    const src = this.getAttribute('src');
+    document.getElementById('fullSizeImage').setAttribute('src', src);
+  });
+})
